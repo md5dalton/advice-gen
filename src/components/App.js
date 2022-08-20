86d1be191d8b55cceddc420c6c80acfa5a4b60b1
md5dalton <md5daltonggs@gmail.com>
2022-08-19 20:28:01 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,40 +10,5 @@ const App = () =>(
     </main>
 )
 
-class Storage
-{
-    store = sessionStorage
-    items = []
-
-    constructor (name) {
-        this.name = name
-    }
-    
-    save () {
-        this.store.setItem(this.name, JSON.stringify(this.items))    
-    }
-    getAll () {
-        const items = this.store.getItem(this.name)
-        this.items = items ? JSON.parse(items) : []
-        return this.items
-    }
-
-    getItem (key) {
-        return this.items[key]
-    }
-    addItem (value) {
-        this.items = [...this.items, value]
-        this.save()
-    }
-}
-class Chat
-{
-    storage = new Storage
-
-    addComment () {
-        this.storage.addItem()
-    }
-}
-
 const root = createRoot(document.getElementById('app'))
 root.render(<App />)
